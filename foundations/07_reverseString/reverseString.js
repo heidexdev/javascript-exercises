82911ae9ba5796f37926bf8cc07a4a3a90845933
heidexdev <heidexdev@gmail.com>
2026-06-19 11:01:36 +0330

    refactor reverseString function to accept a string parameter and work

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string = "") {
+  strArr = string.split("");
+  strArr.reverse();
+  outPut = "";
+  for (char of strArr) {
+    outPut += char;
+  }
+  return outPut;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
