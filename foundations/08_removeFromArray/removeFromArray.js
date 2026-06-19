a71c27061b714723661d1bfbb5a984c1da9aecda
heidexdev <heidexdev@gmail.com>
2026-06-19 11:50:19 +0330

    refactor removeFromArray function to accept an array and multiple arguments , passed the first test

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let array = arr;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === args[0]) {
+      array.splice(i,1);
+    }
+  }
+  return array;
 };
+const result = removeFromArray([1, 2, 3, 4], 3);
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
