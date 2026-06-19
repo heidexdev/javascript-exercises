bb71f96a6aa0b525f7b5b135457adba03ddb5727
heidexdev <heidexdev@gmail.com>
2026-06-19 10:39:59 +0330

    refactor repeatString function to include parameters and improve output handling

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let outPut = "";
+  for (let i = 0; i < num; i++) {
+    outPut += string;
+  }
+  console.log(outPut);
+  return outPut;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
