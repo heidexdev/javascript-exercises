45c47d460573c80bbe3a8188d4989cc3fe52cea3
heidexdev <heidexdev@gmail.com>
2026-06-22 16:30:07 +0330

    refactor:removeFromArray function
    to improve item removal logic , now: removes multiple values (first and second test complete )

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,13 +1,23 @@
 const removeFromArray = function (arr, ...args) {
   let array = arr;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === args[0]) {
-      array.splice(i,1);
+  // for (let i = 0; i < array.length; i++) {
+  //   if (array[i] === args[0]) {
+  //     array.splice(i, 1);
+  //   }
+  // }
+  for (const arg of args) {
+    for (const item of array) {
+      const i = array.indexOf(item);
+      if (arg === item) {
+        array.splice(i, 1);
+      }
     }
   }
+  console.log(args[0], args[1]);
+
   return array;
 };
-const result = removeFromArray([1, 2, 3, 4], 3);
+const result = removeFromArray([1, 2, 2, 4], 2);
 console.log(result);
 
 // Do not edit below this line
