73bc7370c102f5370d646a547419714fcd64857a
heidexdev <heidexdev@gmail.com>
2026-06-22 17:37:30 +0330

    doing the exercise with filter method
    comenting the previus code

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,20 +1,24 @@
 const removeFromArray = function (arr, ...args) {
-  let array = arr;
-  function remove() {
-    for (const arg of args) {
-      for (const item of array) {
-        const i = array.indexOf(item);
-        if (arg === item) {
-          array.splice(i, 1);
-        }
-      }
-      while (array.includes(arg)) {
-        remove();
-      }
-    }
-  }
-  remove();
-  return array;
+  // let array = arr;
+  // function remove() {
+  //   for (const arg of args) {
+  //     for (const item of array) {
+  //       const i = array.indexOf(item);
+  //       if (arg === item) {
+  //         array.splice(i, 1);
+  //       }
+  //     }
+  //     while (array.includes(arg)) {
+  //       remove();
+  //     }
+  //   }
+  // }
+  // remove();
+  // return array;
+  const newArray = arr.filter((item) => {
+    return !args.includes(item);
+  });
+  return newArray;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
