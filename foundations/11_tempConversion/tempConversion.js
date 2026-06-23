8f0dc00c6c01b61124abd094019bad68a711f376
heidexdev <heidexdev@gmail.com>
2026-06-23 22:27:54 +0330

    refactor: remove default parameter values in temperature conversion functions for clarity

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,8 @@
-const convertToCelsius = function (F = Number) {
+const convertToCelsius = function (F) {
   return parseFloat(((F - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function (C = Number) {
+const convertToFahrenheit = function (C) {
   return parseFloat((C * 1.8 + 32).toFixed(1));
 };
 // Do not edit below this line
