142515770933cd86a7046ff0e6ce54fa330f0f5c
heidexdev <heidexdev@gmail.com>
2026-06-23 17:17:27 +0330

    refactor: implement sumAll function to calculate the sum of a range of numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  const sm = a > b ? b : a;
+  const bg = a > b ? a : b;
+  let rangeArray = [];
+  function createRangeArr() {
+    rangeArray.push(sm);
+    for (let i = 1; i <= bg - sm; i++) {
+      rangeArray.push(sm + i);
+    }
+  }
+  createRangeArr();
+  const initialValue = 0;
+  const result = rangeArray.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  result;
+  return result;
 };
+const result = sumAll(2, 4);
+console.log(result);
 
 // Do not edit below this line
 module.exports = sumAll;
