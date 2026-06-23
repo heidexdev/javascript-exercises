4ed1da0c1a23b41cf4033f7518cc574b28014208
heidexdev <heidexdev@gmail.com>
2026-06-23 22:24:38 +0330

    refactor: add parameters to temperature conversion functions for improved clarity

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F = Number) {
+  return parseFloat(((F - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C = Number) {
+  return parseFloat((C * 1.8 + 32).toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
