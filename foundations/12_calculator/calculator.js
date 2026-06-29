79ace4bf2cb5dbbdda8ab8ad6f0305234fa621f1
heidexdev <heidexdev@gmail.com>
2026-06-29 20:06:32 +0330

    refactor: improve formatting and enable subtraction tests for better coverage

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,27 +1,20 @@
-const add = function(a,b) {
+const add = function (a, b) {
   return a + b;
-	
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  
 };
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -30,5 +23,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
