8083763513b9e0ca77fa249e3af765a83b311a36
heidexdev <heidexdev@gmail.com>
2026-06-30 15:24:38 +0330

    refactor: update fibonacci function to accept position argument and improve test cases

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (position) {
+  let prev = 1;
+  let current = 1;
+  let next = 0;
+  for (let i = 3; i <= position; i++) {
+    next = prev + current;
+    prev = current;
+    current = next;
+  }
+  return current;
 };
+const result = fibonacci(5);
+console.log(result);
 
 // Do not edit below this line
 module.exports = fibonacci;
